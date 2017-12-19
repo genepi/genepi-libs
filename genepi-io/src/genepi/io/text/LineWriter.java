63656d5c0de168dcd36db16bf5ba3611952d352e
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2017-12-19 14:55:25 +0100

    count amount of data rows

--- genepi-io/src/genepi/io/text/LineWriter.java
+++ genepi-io/src/genepi/io/text/LineWriter.java
@@ -11,23 +11,36 @@ public class LineWriter {
 
 	private boolean first = true;
 
+	private boolean data = false;
+
 	public LineWriter(String filename) throws IOException {
 		bw = new BufferedWriter(new FileWriter(new File(filename), false));
 		first = true;
+		data = false;
 	}
 
 	public void write(String line) throws IOException {
+		write(line, true);
+	}
+
+	public void write(String line, boolean dataRow) throws IOException {
 		if (first) {
 			first = false;
 		} else {
 			bw.newLine();
 		}
 
+		this.data = dataRow;
+
 		bw.write(line);
 	}
 
 	public void close() throws IOException {
 		bw.close();
+	}
+
+	public boolean hasData() {
+		return data;
 	}
 
 }
