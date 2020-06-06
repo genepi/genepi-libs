e95d7b6bceee62d6cd9b5cd96f7b80ff7c5ac4a7
lukfor <lukas.forer@i-med.ac.at>
2020-06-06 14:12:16 +0200

    Add UID to exception

--- genepi-io/src/genepi/io/table/exceptions/ColumnNotFoundException.java
+++ genepi-io/src/genepi/io/table/exceptions/ColumnNotFoundException.java
@@ -2,6 +2,8 @@ package genepi.io.table.exceptions;
 
 public class ColumnNotFoundException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public ColumnNotFoundException(String message) {
 		super(message);
 	}
